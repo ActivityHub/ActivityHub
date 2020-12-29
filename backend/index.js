776197324684ac401d6f79669688cd47ab446eb8
Sebastian <sebastian.chang23@gmail.com>
2020-12-29 11:05:07 -0600

    Setting up Twilio with our application

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,15 @@ const logger = require('./lib/logger')
 const router = require('./config/router')
 const errorHandler = require('./lib/errorHandler')
 const { dbURI, port } = require('./config/environment')
+const path = require('path');
+const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+require('dotenv').config();
 
+const MAX_ALLOWED_SESSION_DURATION = 14400;
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
+const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
 
 mongoose.connect(
   dbURI,
@@ -19,6 +27,13 @@ mongoose.connect(
   }
 )
 
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+//   app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
+//   })
+// }
 
 app.use(express.static(`${__dirname}/frontend/build`))
 
@@ -26,9 +41,20 @@ app.use(express.json())
 
 app.use(logger)
 
+// app.get('/token', (req, res) => {
+//   const { identity, roomName } = req.query;
+//   const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
+//     ttl: MAX_ALLOWED_SESSION_DURATION,
+//   });
+//   token.identity = identity;
+//   const videoGrant = new VideoGrant({ room: roomName });
+//   token.addGrant(videoGrant);
+//   res.send(token.toJwt());
+//   console.log(`issued token for ${identity} in room ${roomName}`);
+// });
+
 app.use('/api', router)
 
-app.use('/*', (_, res) => res.sendFile(`${__dirname}/frontend/build/index.html`))
 
 app.use(errorHandler)
 
